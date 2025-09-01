e36ea4561187e24564f1140cf617d53760ffc35a
vladionolteanu-star <vladionolteanu@gmail.com>
2025-09-01 12:55:58 +0300

    Create EchoMarker.tsx

---
+++ src/components/Echo/EchoMarker.tsx
@@ -0,0 +1,11 @@
+import { Marker } from 'react-map-gl'
+import { motion } from 'framer-motion'
+
+interface EchoMarkerProps {
+  echo: any // Will be properly typed later
+}
+
+export default function EchoMarker({ echo }: EchoMarkerProps) {
+  // Placeholder for now - echoes will be implemented later
+  return null
+}
