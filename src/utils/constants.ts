eb170e9b117252d40ff4405b21094ab85a1c8082
vladionolteanu-star <vladionolteanu@gmail.com>
2025-09-02 22:44:33 +0300

    Update constants.ts

--- src/utils/constants.ts
+++ src/utils/constants.ts
@@ -13,10 +13,10 @@ export const STAR_COLORS = [
   '#F472B6'  // Pink
 ]
 
-export const CONNECTION_DISTANCE = 500 // meters
+export const CONNECTION_DISTANCE = 500000 // meters
 export const MAX_CONNECTIONS = 7
 export const POSITION_UPDATE_INTERVAL = 3000 // ms
-export const STATIONARY_TIME = 5000 // ms before showing echo button
+export const STATIONARY_TIME = 50000 // ms before showing echo button
 
 export const generateStarColor = () => {
   return STAR_COLORS[Math.floor(Math.random() * STAR_COLORS.length)]
