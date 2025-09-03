74b29a35d98084c06dc4a03aa4ff4a67c7028d3a
vladionolteanu-star <vladionolteanu@gmail.com>
2025-09-04 00:02:54 +0300

    Update ConstellationLines.tsx

--- src/components/map/ConstellationLines.tsx
+++ src/components/map/ConstellationLines.tsx
@@ -1,8 +1,1 @@
-import React from 'react';
 
-// Component simplu fără dependințe problematice
-const ConstellationLines: React.FC = () => {
-  return null; // Component dezactivat temporar
-};
-
-export default ConstellationLines;
