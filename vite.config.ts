04135f5a6cae476a106f189560682d4e8ead804b
vladionolteanu-star <vladionolteanu@gmail.com>
2025-09-03 17:38:14 +0300

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -4,14 +4,24 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   optimizeDeps: {
-    exclude: ['mapbox-gl']
+    include: ['mapbox-gl', 'supercluster'],
+    exclude: []
   },
   build: {
     rollupOptions: {
-      external: [],
       output: {
-        manualChunks: undefined
+        manualChunks: {
+          'mapbox': ['mapbox-gl', 'react-map-gl', 'supercluster'],
+          'vendor': ['react', 'react-dom'],
+          'supabase': ['@supabase/supabase-js'],
+        }
       }
-    }
+    },
+    sourcemap: false,
+    minify: 'esbuild',
+    target: 'es2020'
+  },
+  esbuild: {
+    logOverride: { 'this-is-undefined-in-esm': 'silent' }
   }
 })
